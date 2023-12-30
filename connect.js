bec409720adcc1f56d66f4546b7df677c42b6d35
Ev Bogue <ev@evbogue.com>
2023-12-30 13:00:54 -0600

    request all current feeds from server to get latest if any

--- connect.js
+++ connect.js
@@ -1,14 +1,23 @@
 import { process } from './process.js'
 import { ed25519 } from './keys.js'
 import { addSocket, rmSocket} from './gossip.js'
+import { logs } from './log.js'
 
 export const connect = (s) => {
   const ws = new WebSocket(s)
   ws.binaryType = 'arraybuffer'
 
   ws.onopen = async () => {
+    const pubkey = await ed25519.pubkey()
     addSocket(ws)
-    ws.send(await ed25519.pubkey())
+    ws.send(pubkey)
+
+    const feeds = await logs.getFeeds()
+    feeds.forEach(feed => {
+      if (feed != pubkey) {
+        ws.send(feed)
+      }
+    })
   }
 
   ws.onmessage = async (e) => {
