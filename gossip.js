de57eb9afdd4f94aa33df8686c0bf0e59e6b4dca
Ev Bogue <ev@evbogue.com>
2024-02-29 20:25:14 -0600

    keep track of msgs sent so we do not loop

--- gossip.js
+++ gossip.js
@@ -1,21 +1,31 @@
 import { trystero } from './trystero.js'
 
+let queue = []
+
 const sockets = new Set()
 
 export const gossip = (msg) => {
-  console.log(msg)
-  if (trystero.send) {
-    trystero.send(msg)
-  } else {
-    setTimeout(() => {
-      trystero.send(msg)
-    }, 1000)
-  }
+  queue.push(msg)
 
-  if (sockets.length) { 
-    sockets.forEach(s => s.send(JSON.stringify(msg)))
-  } else {
-    setTimeout(function () { sockets.forEach(s => s.send(JSON.stringify(msg))) }, 500)
+  setInterval(() => {
+    queue = []
+  }, 10000)
+  console.log(queue)
+  if (!queue.includes(msg)) {
+    console.log(msg)
+    if (trystero.send) {
+      trystero.send(msg)
+    } else {
+      setTimeout(() => {
+        trystero.send(msg)
+      }, 1000)
+    }
+
+    if (sockets.length) { 
+      sockets.forEach(s => s.send(JSON.stringify(msg)))
+    } else {
+      setTimeout(function () { sockets.forEach(s => s.send(JSON.stringify(msg))) }, 500)
+    }
   }
 }
 
