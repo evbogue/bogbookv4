4623b9f504f8e246a6be0b3fe61724d7336f8cd3
Ev Bogue <ev@evbogue.com>
2024-03-01 12:06:58 -0600

    we no longer set name on the settings page

--- settings.js
+++ settings.js
@@ -3,37 +3,8 @@ import { ed25519 } from './keys.js'
 import { cachekv } from './lib/cachekv.js'
 
 const keypair = await ed25519.keypair()
-const pubkey = await ed25519.pubkey() 
-
-const getPrevious = await cachekv.get(pubkey)
-
-let previous
-
-if (getPrevious) { previous = JSON.parse(getPrevious)} else { previous = {} }
-
-const input = h('input')
-
-if (previous && previous.name) {
-  input.placeholder = previous.name
-}
 
 const textarea = h('textarea', [keypair])
-
-const saveButton = h('button', {
-  onclick: async () => {
-    if (textarea.value && textarea.value.length === keypair.length) {
-      await localStorage.setItem('keypair', textarea.value)      
-    } else { 
-      alert('Invalid Keypair')
-    }
-    if (input.value) {
-      previous.name = input.value
-      await cachekv.put(pubkey, JSON.stringify(previous))
-    }
-    location.href = '#'
-    location.reload()
-  }
-}, ['Save'])
 
 const deleteKeypair = h('button', {
   style: 'float: right;',
@@ -54,10 +25,19 @@ const deleteEverything = h('button', {
   }
 }, ['Delete Everything'])
 
+const saveButton = h('button', {
+  onclick: async () => {
+    if (textarea.value && textarea.value.length === keypair.length) {
+      await localStorage.setItem('keypair', textarea.value)
+    } else {
+      alert('Invalid Keypair')
+    }
+    location.href = '#'
+    location.reload()
+  }
+}, ['Save'])
+
 export const settings = h('div', {classList: 'message'}, [
-  'Name: ', 
-  input,
-  h('br'),
   'Keypair:',
   h('br'),
   textarea,
