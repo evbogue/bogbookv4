36cb3af4dbf17daef9e485f9ab23132da5651ea5
Ev Bogue <ev@evbogue.com>
2024-03-08 10:11:55 -0600

    store latest in memory for speedier avatars

--- getinfo.js
+++ getinfo.js
@@ -1,25 +1,35 @@
 import { cachekv } from './lib/cachekv.js'
 import { logs } from './log.js'
 
+const info = new Map()
+
 export const getInfo = async (pubkey) => {
-  let obj = {}
-  
-  const getString = await cachekv.get(pubkey)
+  if (info.has(pubkey)) {
+    console.log(pubkey + ' is in memory.')
+    return info.get(pubkey)
+  } else {
+    console.log(pubkey + ' is not in memory.')
+    let obj = {}
 
-  if (getString)
-    obj = JSON.parse(getString)
+    const getString = await cachekv.get(pubkey)
 
-  if (!obj.payload) {
-    const getLatest = await logs.getLatest(pubkey)
-    if (getLatest) {
-      obj.payload = getLatest.raw
+    if (getString)
+      obj = JSON.parse(getString)
+      info.set(pubkey, obj)
+    if (!obj.payload) {
+      const getLatest = await logs.getLatest(pubkey)
+      if (getLatest) {
+        obj.payload = getLatest.raw
+        info.set(pubkey, obj)
+      }
     }
+    info.set(pubkey, obj)
+    return obj
   }
-
-  return obj
 }
 
 export const saveInfo = async (pubkey, data) => {
   console.log(data)
+  info.set(pubkey, data)
   await cachekv.put(pubkey, JSON.stringify(data))
 }
